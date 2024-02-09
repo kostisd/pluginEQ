0575b9add4975a5fb36a3d85843274c9a0a6600f
Kostis Dimos <kostisd83@gmail.com>
2024-02-09 21:02:32 +0200

    Source first commit

---
+++ Source/PluginEditor.h
@@ -0,0 +1,33 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class PluginEQAudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    PluginEQAudioProcessorEditor (PluginEQAudioProcessor&);
+    ~PluginEQAudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    PluginEQAudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (PluginEQAudioProcessorEditor)
+};
